ed8deb5edb0c22e3948c414198f4a7be2d588d58
alexisyanez <adanilo.yanez@gmail.com>
2024-04-12 17:15:14 -0400

    agregando mas gráficas

--- app.py
+++ app.py
@@ -6,18 +6,48 @@ import plotly.express as px
 def main():
     st.set_page_config(layout='wide')
     st.title('Results visualization for OOP for SPS')
-    df = load_data()
-    with st.expander('Show data'):
-        st.write(df)
-    with st.expander('Some plots', expanded=True):
+
+    path_data1 = 'OOP_for_SPS/Final_results_group1.json'
+    df_1 = load_data(path_data1)
+
+    path_data2 = 'OOP_for_SPS/Final_results_group2_50Hz.json'
+    df_2 = load_data(path_data2)
+
+    path_data3 = 'OOP_for_SPS/Final_results_group3_10Hz.json'
+    df_3 = load_data(path_data3)
+
+    path_data4 = 'OOP_for_SPS/Final_results_group4_Fitting_20Hz.json'
+    df_4 = load_data(path_data4)
+
+    with st.expander('Show data 10 Hz'):
+        st.write(df_3)
+    with st.expander('Some plots 10 Hz', expanded=True):
         #lets filter by obstacles and nr
         obstacles = st.toggle('Obstacles', False)
-        nr = st.toggle('NR', False)
-        plot_scatter(df, obstacles, nr)
+        #nr = st.toggle('NR', False)
+        plot_scatter(df_3, obstacles)
 
+    with st.expander('Some plots 20 Hz ', expanded=True):
+        #lets filter by obstacles and nr
+        obstacles = st.toggle('Obstacles', False)
+        #nr = st.toggle('NR', False)
+        plot_scatter(df_1, obstacles)
 
-def plot_scatter(df, obstacles, nr):
-    df_toplot = (df.query(f'obstacles == {obstacles} and nr == {nr}')
+    with st.expander('Some plots 50 Hz', expanded=True):
+        #lets filter by obstacles and nr
+        obstacles = st.toggle('Obstacles', False)
+        #nr = st.toggle('NR', False)
+        plot_scatter(df_2, obstacles)
+
+    with st.expander('Some plots 4', expanded=True):
+        #lets filter by obstacles and nr
+        obstacles = st.toggle('Obstacles', False)
+        # nr = st.toggle('NR', False)
+        plot_scatter2(df_4, obstacles)
+
+def plot_scatter(df, obstacles): #,nr):
+    #df_toplot = (df.query(f'obstacles == {obstacles} and nr == {nr}')
+    df_toplot = (df.query(f'obstacles == {obstacles}')
                    .melt(value_vars=['emp_VAP_avg'], id_vars=['target_distance', 'awareness_window'])
                    .sort_values('awareness_window'))
     fig = px.scatter(df_toplot, x='target_distance', y='value', color='awareness_window', symbol='awareness_window', title='VAP vs Tx-Rx Distance')
@@ -25,10 +55,18 @@ def plot_scatter(df, obstacles, nr):
     fig.update_yaxes(title_text='VAP')
     st.plotly_chart(fig)
 
+def plot_scatter2(df, obstacles):
+    df_toplot = (df.query(f'obstacles == {obstacles}')
+                   .melt(value_vars=['All_indv_emp_VAP[:]'], id_vars=['All_indv_VRU_AVGPD[:]'])) 
+    fig = px.scatter(df_toplot, x='All_indv_VRU_AVGPD[:]', y='value', color='awareness_window', symbol='awareness_window', title='VAP vs Tx-Rx Distance')
+    fig.update_xaxes(title_text='Tx-Rx Distance (m)')
+    fig.update_yaxes(title_text='VAP')
+    st.plotly_chart(fig)    
 
-def load_data():
+
+def load_data(data_path):
     # I need to read some json files
-    with open('OOP_for_SPS/Final_results_group1.json') as f:
+    with open(data_path) as f:
         data = json.load(f)
     return pd.DataFrame(data).T
 
