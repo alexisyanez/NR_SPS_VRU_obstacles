6bec46a43419debbd0fa4e4d5060537b514300ee
alexisyanez <adanilo.yanez@gmail.com>
2024-04-14 16:31:50 -0400

    unnest function

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 import plotly.express as px
 import itertools
+import numpy as np
 
 def main():
     st.set_page_config(layout='wide')
@@ -59,143 +60,34 @@ def plot_scatter(df, obstacles): #,nr):
     st.plotly_chart(fig)
 
 def plot_scatter2(df, obstacles):
-    
-    a=df['All_indv_VRU_AVGPDR'][0]
-    b=df['All_indv_emp_VAP'][0]
-    fig = px.scatter(x=a, y=b,title='VAP vs PDR_VRU_AVG')
+    #df = pd.DataFrame({'A': [1, 2], 'B': [[1, 2], [3, 4]], 'C': [[1, 2], [3, 4]]})
+    df2=unnest(df, ['All_indv_VRU_AVGPDR', 'All_indv_emp_VAP', 'density_scenario','obstacles'])  # multiple columns
    
-    fig.update_xaxes(title_text='VRU PDR AVG')
+    df_toplot = (df2.query(f'obstacles == {obstacles}')
+                   .melt(value_vars=['All_indv_emp_VAP'], id_vars=['All_indv_VRU_AVGPDR', 'density_scenario'])
+                   .sort_values('density_scenario'))
+    fig = px.scatter(df_toplot, x='All_indv_VRU_AVGPDR', y='value', color='density_scenario', symbol='density_scenario', title='VAP vs AVG PDR Sourronding VRU')
+    fig.update_xaxes(title_text='AVG PDR VRU')
     fig.update_yaxes(title_text='VAP')
-    
-    for i in range(1, 6):  # Itera desde 1 hasta 5
-        a_i = df['All_indv_VRU_AVGPDR'][i]
-        b_i = df['All_indv_emp_VAP'][i]
-        fig.add_trace(px.scatter(x=a_i, y=b_i, mode='markers', name=f'Traza {i}').data[0])
-    
     st.plotly_chart(fig)
-
-    #st.plotly_chart(fig)
-
-    #df_toplot = (df.query(f'obstacles == {obstacles} and nr == {nr}')
-    #fig = px.scatter(x=a, y=b,title='VAP vs PDR_VRU_AVG')
-    #fig.update_layout(showlegend=True)
-    #for i in range(1,5):
-    #    a=df['All_indv_VRU_AVGPDR'][i]
-    #    b=df['All_indv_VRU_AVGPDR'][i]
-    #    fig.add_trace(px.scatter(x=a, y=b))
     
-    
-# Flatten the lists
-    #df_expanded = pd.concat([df.explode('All_indv_emp_VAP'), df.explode('All_indv_VRU_AVGPDR')])
-
-    # Create the scatter plot
+    #print()
+    #df_expand=df.explode('')
     #a=df['All_indv_VRU_AVGPDR'][0]
     #b=df['All_indv_emp_VAP'][0]
-    #fig = px.scatter(x=a, y=b, title='VAP vs PDR VRU average')
-    #fig.update_xaxes(range=[0, 1], title_text='PDR')
-    #fig.update_yaxes(range=[0, 1], title_text='VAP')
-
-    # Display the plot using Streamlit
-    #st.plotly_chart(fig)
-
-
-
-    #df_expanded = pd.concat([df.explode('All_indv_emp_VAP'),df.explode('All_indv_VRU_AVGPDR')])   
-    #df_toplot = (df_expanded.query(f'obstacles == {obstacles}')
-    #                .melt(value_vars=['All_indv_emp_VAP'], id_vars=['All_indv_VRU_AVGPDR', 'density_scenario'])
-    #                .sort_values('density_scenario'))
-
-    # Crear el gráfico de dispersión con los valores float
-    #fig = px.scatter(df_toplot, x='All_indv_VRU_AVGPDR', y='value', color='density_scenario', symbol='density_scenario', title='VAP vs PDR VRU average')
-    #fig.update_xaxes(range=[0, 1], title_text='PDR')
-    #fig.update_yaxes(range=[0, 1], title_text='VAP')
-
-    #st.plotly_chart(fig)
-
-
-    # Crear el DataFrame para el gráfico de dispersión
-    #df_toplot = df.explode('All_indv_emp_VAP').melt(value_vars=['All_indv_emp_VAP'], id_vars=['All_indv_VRU_AVGPDR', 'density_scenario']).sort_values('density_scenario')
-
-    # Crear el gráfico de dispersión con los valores float
-    #fig = px.scatter(df_toplot, x='All_indv_VRU_AVGPDR', y='value', color='density_scenario', symbol='density_scenario', title='VAP vs PDR VRU average')
-    #fig.update_xaxes(range=[0, 1], title_text='PDR')
-    #fig.update_yaxes(range=[0, 1], title_text='VAP')
-    #st.plotly_chart(fig)
-    
-    #df_expanded = pd.concat([df.explode('All_indv_emp_VAP'), df.explode('All_indv_VRU_AVGPDR')])
-
-    # Crear el gráfico de dispersión con los valores float
-    #fig = px.scatter(df_expanded, x='All_indv_VRU_AVGPDR', y='All_indv_emp_VAP', title='VAP vs PDR VRU average')
-    #fig.update_xaxes(range=[0, 1], title_text='PDR')
-    #fig.update_yaxes(range=[0, 1], title_text='VAP')
-    #st.plotly_chart(fig)
-
-
-    #df['All_indv_combined_emp'] = df['All_indv_emp_VAP'].apply(lambda x: list(itertools.chain.from_iterable(x)))
-    #df['All_indv_combined_VRU'] = df['All_indv_VRU_AVGPDR'].apply(lambda x: list(itertools.chain.from_iterable(x)))
-# Crear el DataFrame para el gráfico de dispersión
-    #df_toplot = df.explode('All_indv_combined_emp').melt(value_vars=['All_indv_combined_emp'], id_vars=['All_indv_combined_VRU', 'density_scenario']).sort_values('density_scenario')
-
-# Crear el gráfico de dispersión con los valores float
-    #fig = px.scatter(df_toplot, x='All_indv_combined_VRU', y='value', color='density_scenario', symbol='density_scenario', title='VAP vs PDR VRU average')
-    #fig.update_xaxes(range=[0, 1], title_text='PDR')
-    #fig.update_yaxes(range=[0, 1], title_text='VAP')
-    #st.plotly_chart(fig)
-
-    # Concatenate 'All_indv_emp_VAP' and 'All_indv_VRU_AVGPDR' separately
+    #fig = px.scatter(x=a, y=b,title='VAP vs PDR_VRU_AVG')
    
-
-    #df['All_indv_combined_emp'] = pd.to_numeric(df['All_indv_combined_emp'], errors='coerce')
-    #df['All_indv_combined_VRU'] = pd.to_numeric(df['All_indv_combined_VRU'], errors='coerce')
-
-    #with st.expander('Show data 20 Hz Fitting Results'):
-    #st.write(df)
-    # Melt the DataFrame
-    #df_toplot = df.explode('All_indv_combined_emp').melt(value_vars=['All_indv_combined_emp'], id_vars=['All_indv_combined_VRU', 'density_scenario']).sort_values('density_scenario')
-    #df_toplot = (df.query(f'obstacles == {obstacles}')
-    #               .melt(value_vars=['All_indv_emp_VAP'], id_vars=['All_indv_VRU_AVGPDR', 'density_scenario'])
-    #               .sort_values('density_scenario'))
-    
-    # Create the scatter plot
-    #fig = px.scatter(df_toplot, x='All_indv_VRU_AVGPDR', y='value', color='density_scenario', symbol='density_scenario', title='VAP vs PDR VRU average') # trendline='lowess',
-    #fig.update_xaxes(range=[0, 1], title_text='PDR')
-    #fig.update_yaxes(range=[0, 1], title_text='VAP')
-    #st.plotly_chart(fig)
-    
-    # Convertir la cadena en una lista real
-    #df_toplot = (df.query(f'obstacles == {obstacles}')
-    #                .melt(value_vars=['All_indv_emp_VAP'], id_vars=['All_indv_VRU_AVGPDR', 'density_scenario'])
-    #                .sort_values('density_scenario'))
- 
-    #fig = px.scatter(df_toplot, x='All_indv_VRU_AVGPDR', y='value', trendline='lowess', color='density_scenario', symbol='density_scenario', title='VAP vs PDR VRU average')
-    #fig.update_xaxes(range=[0, 1],title_text='PDR')
-    #fig.update_yaxes(range=[0, 1],title_text='VAP')
-    #st.plotly_chart(fig)
-
-    #df['All_indv_emp_VAP'] = df['All_indv_emp_VAP'].apply(ast.literal_eval)
-    #a = df['All_indv_emp_VAP'].iloc[0]
-    
-    #df['All_indv_VRU_AVGPDR'] = df['All_indv_VRU_AVGPDR'].apply(ast.literal_eval)
-    #b=df['All_indv_VRU_AVGPDR'].iloc[0]
-
-    #a_concatenated = [item for sublist in a for item in sublist]
-    #b_concatenated = [item for sublist in b for item in sublist]
-    # Crear el gráfico de dispersión con Plotly
-    #fig = px.scatter(x=b[0], y=[0], title='VAP vs Tx-Rx Distance')
-    #fig.update_xaxes(title_text='Tx-Rx Distance (m)')
+    #fig.update_xaxes(title_text='VRU PDR AVG')
     #fig.update_yaxes(title_text='VAP')
-
-    # Mostrar el gráfico
+          
     #st.plotly_chart(fig)
 
-#def plot_scatter2(df, obstacles):
-#    df_toplot = [df['All_indv_emp_VAP'][0],['All_indv_VRU_AVGPDR'][0]]#(df.query(f'obstacles == {obstacles}')
-                #   .melt(value_vars=['All_indv_emp_VAP'][0], id_vars=['All_indv_VRU_AVGPDR'][0])) 
-#    fig = px.scatter(df_toplot, x='All_indv_VRU_AVGPDR', y='All_indv_emp_VAP', title='VAP vs Tx-Rx Distance')
-#    fig.update_xaxes(title_text='Tx-Rx Distance (m)')
-#    fig.update_yaxes(title_text='VAP')
-#    st.plotly_chart(fig)    
+def unnest(frame, explode):
+    def mesh(values):
+        return np.array(np.meshgrid(*values)).T.reshape(-1, len(values))
 
+    data = np.vstack(mesh(row) for row in frame[explode].values)
+    return pd.DataFrame(data=data, columns=explode)
 
 def load_data(data_path):
     # I need to read some json files
