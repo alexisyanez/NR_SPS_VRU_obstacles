e534f140d68ae05b75a5f8668abc739c16b552ab
alexisyanez <adanilo.yanez@gmail.com>
2024-04-19 10:30:05 -0400

    updating last figures witth bars

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from plotly.subplots import make_subplots
 import statsmodels.api as sm
 import numpy as np
 from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 
 def main():
@@ -55,6 +55,32 @@ def main():
         #nr = st.toggle('NR', False)
         plot_scatter(df_2, obstacles2)
 
+    with st.expander('Scatter and fittings figures', expanded=True):
+        # Sample data
+        x_values = np.linspace(0, 6, 7)
+        y_values_no_obs = [df_4['VRU_PDR_avg'][0],df_5['VRU_PDR_avg'][1],df_5['VRU_PDR_avg'][2],df_5['VRU_PDR_avg'][5],df_5['VRU_PDR_avg'][7],df_6['VRU_PDR_avg'][0],df_6['VRU_PDR_avg'][1]]
+        error_values_no_obs = [df_4['VRU_PDR_std'][0],df_5['VRU_PDR_std'][1],df_5['VRU_PDR_std'][2],df_5['VRU_PDR_std'][5],df_5['VRU_PDR_std'][7],df_6['VRU_PDR_std'][0],df_6['VRU_PDR_std'][1]]
+
+        
+        y_values_obs = [df_4['VRU_PDR_avg'][3],df_5['VRU_PDR_avg'][4],df_5['VRU_PDR_avg'][6],df_5['VRU_PDR_avg'][8],df_5['VRU_PDR_avg'][9],df_6['VRU_PDR_avg'][2],df_6['VRU_PDR_avg'][3]]
+        error_values_obs = [df_4['VRU_PDR_std'][3],df_5['VRU_PDR_std'][4],df_5['VRU_PDR_std'][6],df_5['VRU_PDR_std'][8],df_5['VRU_PDR_std'][9],df_6['VRU_PDR_std'][2],df_6['VRU_PDR_std'][3]]
+        
+        fig3 = make_subplots(rows=1, cols=1)
+        # Create the bar chart with error bars
+        
+        fig3.add_trace(go.Bar(x=x_values, y=y_values_no_obs, error_y=dict(type='data', array=error_values_no_obs, visible=True), name='no_obs'), row=1, col=1)
+        fig3.add_trace(go.Bar(x=x_values, y=y_values_obs, error_y=dict(type='data', array=error_values_obs, visible=True), name='obs'), row=1, col=1)
+
+        fig3.update_layout(title='Bar Plot Figures VRU AVGPDR, density_scenario = [0,1,2,3,4,5,6]',
+                        autosize=False,
+                        width=600,
+                        height=500)
+                        #legend=dict(x=0.3, y=0.1))
+       
+        st.plotly_chart(fig3)
+        
+
+
     with st.expander('Show data 20 Hz Fitting Results'):
         st.write("First group results")
         st.write(df_4)
@@ -62,14 +88,15 @@ def main():
         st.write(df_5)
         st.write("Third group results")
         st.write(df_6)
-    with st.expander('Some plots 4', expanded=True):
+
+    with st.expander('Scatter and fittings figures', expanded=True):
         
         st.write("Scatter plot for Density Scenario=6")
         plot_scatter2(df_4,0,3,6)
         st.write("Scatter plot for Density Scenario=5")
         plot_scatter2(df_5,1,4,5)
         st.write("Scatter plot for Density Scenario=4")
-        plot_scatter2(df_5,2,5,4)
+        plot_scatter2(df_5,2,6,4)
         st.write("Scatter plot for Density Scenario=3")
         plot_scatter2(df_5,5,8,3)
         st.write("Scatter plot for Density Scenario=2")
@@ -78,7 +105,6 @@ def main():
         plot_scatter2(df_6,0,2,1)
         st.write("Scatter plot for Density Scenario=0")
         plot_scatter2(df_6,1,3,0)
-
 
 def plot_scatter(df, obstacles): #,nr):
     #df_toplot = (df.query(f'obstacles == {obstacles} and nr == {nr}')
@@ -109,27 +135,28 @@ def plot_scatter2(df,no_obs,obs,ds):
     fig2.add_trace(go.Scatter(x=lowess_2[:, 0], y=lowess_2[:, 1], mode='lines', name='LOWESS Fit for no_obs', line=dict(color='blue')))
     
     # Fit the model to the data
-    initial_guess = [1, 1, 1]  # Initial parameter guesses
-    limites_inferiores = [0, 0, 0]
-    limites_superiores = [2, 2, 2]
+    initial_guess = [1, 1, 1, 2]  # Initial parameter guesses
+    limites_inferiores = [0, 1, 1, 0]
+    limites_superiores = [2, 2, 2, 8]
     limites = (limites_inferiores, limites_superiores)
     optimized_params, _ = curve_fit(my_model,df['All_indv_VRU_AVGPDR'][obs], df['All_indv_emp_VAP'][obs], p0=initial_guess, bounds=limites)
-    
-    st.write("Parametros Optimizados",optimized_params)
+
+       
+    st.write("Obtimized Parameters for model $VAP = A-B (C-PDR_{VRU})^z$",optimized_params)
 
     # Plot the data and the fitted curve
-    fig2.add_trace(go.Scatter(x=df['All_indv_VRU_AVGPDR'][obs], y=my_model(df['All_indv_VRU_AVGPDR'][obs], *optimized_params), mode='lines', name='Fitted Curve model', line=dict(color='orange')))
+    fig2.add_trace(go.Scatter(x=df['All_indv_VRU_AVGPDR'][obs], y=my_model(df['All_indv_VRU_AVGPDR'][obs], *optimized_params), mode='lines', name='Fitted Curve model for obs', line=dict(color='orange')))
 
     fig2.update_layout(title='Scatter Plot Figures VAP v/s VRU AVGPDR, density_scenario = 1, LOWESS Fit for obs',
                         autosize=False,
-                        width=400,
-                        height=400,
-                        legend=dict(x=0.3, y=0.1))
+                        width=600,
+                        height=500)
+                        #legend=dict(x=0.3, y=0.1))
        
     st.plotly_chart(fig2)
 
-def my_model(x, a,c,b):
-    return a-b*((c-x)**2)
+def my_model(x, a,b,c,z):
+    return a-b*((c-x)**z)
 
 def load_data(data_path):
     # I need to read some json files
