44608335c84fb0f0142f98b2402d2879a1072a08
alexisyanez <adanilo.yanez@gmail.com>
2025-02-19 14:39:17 -0500

    Cluster graphs

--- app_cluster.py
+++ app_cluster.py
@@ -15,54 +15,93 @@ def main():
     #i=0
 
     #path_data_1 = 'OOP_for_SPS/Final_results_Cluster0.json'
-    path_data_1 = 'OOP_for_SPS/Final_results_noCluster_0_28-11-91.json'
-    df_1 = load_data(path_data_1)
-    df_1 = df_1[df_1['density_scenario'] == 15]
     
 
-    path_data_2 = 'OOP_for_SPS/Final_results_Cluster_v15.json'
-    df_2 = load_data(path_data_2)
-    df_2 = df_2[df_2['max_speed_diff'] == 15]
-    df_2 = df_2[df_2['min_cl'] == 2]
-    
+    #ds = 19
+    #mcl = 2
+
+    for ds in range(15,20):
+        for mcl in range(2,7):
+            path_data_1 = 'OOP_for_SPS/Final_results_noCluster_0_28-11-91.json'
+            df_1 = load_data(path_data_1)
+
+            path_data_2 = 'OOP_for_SPS/merged.json'
+            df_2 = load_data(path_data_2)
+            
+
+            df_1 = df_1[df_1['density_scenario'] == ds]
+            
+            df_2 = df_2[df_2['max_speed_diff'] == 15]
+            df_2 = df_2[df_2['min_cl'] == mcl]
+            df_2 = df_2[df_2['density_scenario'] == ds]
+            
+
+            df_3 = pd.concat([df_1,df_2])
+
+
+            plot_box(df_3,ds,mcl)
+    #path_data_2 = 'OOP_for_SPS/Final_results_Cluster_v15.json'
     #df_2 = load_data(path_data_2)
+    #df_2 = df_2[df_2['max_speed_diff'] == 15]
+    #df_2 = df_2[df_2['min_cl'] == 2]
+
+    #min_cl=[2,3,4,5,6] x*5 -> 10,15,20,25,30
+    #max_cl=[10,15,20,25,30]
+    #max_speed_diff_list=[5,10,15] 
+    #max_distance_cl=[3,4,5,10,15]
+
+    
+    
+    
+
+    #df_2 = load_data(path_data_2)
+    # Possibles combinaciones
 
 
     #print(df_1.head())
     #print(df_2.head())
 
     #print(df_1.columns)
-    print(df_2.columns)
-    print(df_2['clusters_info'][0][0])
+    #print(df_2.columns)
+    #print(df_2['clusters_info'][0][0])
 
-    df_3 = pd.concat([df_1,df_2])
-
+    
     #df_3.reset_index(drop=True, inplace=True)
     #df_3['cluster'][0]=False
 
     #print(df_3['All_PDR_Vector'])
-    print(df_3['clusters_info'][1][1])
+    #print(df_3['clusters_info'][1][1])
 
-    plot_box(df_3)
-
-def plot_box(df):
-    df['index'] = df.index   
     
-    df=df.explode(['All_PDR_Vector'])                                  
-     
+
+def plot_box(df,ds,mcl):
+    #print(df['index'])
+    df['index'] = df.index      
+    
+    df = df.sort_values(by='index') 
+    df = df.sort_values(by='cluster') 
+    
+    #df=df.explode(['All_indv_VRU_AVGPDR'])         
+                      
+    df=df.explode(['All_PDR_Vector'])   
+    
     #Crear el gráfico Boxplot color='density_scenario',
     fig = px.box(df, x='index', y='All_PDR_Vector', color='cluster' , notched=True,
-                title='Boxplot of ALL PDR Average by Clustering in Density Scenario 15<br>for max_speed_diff = 15%, Min Member = 2 and Max Member = 10 ',
+                title='ALL PDR Average in Density Scenario '+ str(ds-14) +'<br> for max_speed_diff = 15%, Min Member = '+str(mcl) + ' and Max Member = '+str(mcl*5),
                 labels={'ALL_PDR_avg': 'Average ALL_PDR', 'index': 'Index'},
-                points='all')  # 'all' to show all points
-
-    
+                points='all')  # 'all' to show all points    
     #fig = px.scatter(df,x='index', y='ALL_PDR_avg', range_y=[0, 1], color='max_cl', title='Error Bar Plot') 
-    fig.update_layout(xaxis_title='Maximum Distance for Cluster member [m]', yaxis_title='Average ALL_PDR', font_size=16)
-    fig.update_xaxes(tickvals=[9, 11, 12, 13, 14], ticktext=['3', '4', '5', '10', '15'])
+    fig.update_layout(xaxis_title='Maximum Distance for Cluster member [m]', yaxis_title='Average ALL_PDR', font_size=12, title_x=0.5,)
+    #val = df['index'].unique()
+    #print(val)
+    fig.update_xaxes(tickvals=df['index'].unique(), ticktext=['N/A','3', '4', '5', '10', '15'])
+    fig.update_yaxes(range=[0.5,1])
 
 
-    fig.show()       
+    name="images/cluster_ds_"+str(ds-14)+"_mincl_"+str(mcl)+"_PDRALLL.pdf"
+    fig.write_image(name, format='pdf')
+    
+    #fig.show()       
     
     #fig.write_image(name, format='pdf')
 
