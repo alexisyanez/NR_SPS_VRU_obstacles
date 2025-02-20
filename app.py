3fb2f802f10080ece89ce9f82f69a2c53e97b50b
alexisyanez <adanilo.yanez@gmail.com>
2025-02-19 20:55:32 -0500

    more graphs

--- app.py
+++ app.py
@@ -99,12 +99,63 @@ def main():
             title_x=0.5
         )
 
+        fig.update_xaxes(tickvals=df_2['max_dist_clust'].unique(), ticktext=['3', '4', '5', '10', '15'])
+        fig.update_yaxes(range=[0.5,1])
+
         st.plotly_chart(fig)
 
         
         #plot_box(df_2)
 
         st.write("Density Scenario 16")
+
+        df_3=df_3.explode(['All_PDR_Vector'])   
+
+        # Slider for selecting max_speed_diff
+        selected_speed_2 = st.select_slider(
+            label='Select max_speed_diff', 
+            options=[5, 10, 15], 
+            value=10,
+            key='slider_speed'
+        )
+
+        # Filter the DataFrame based on the slider value
+        filtered_df_2 = df_3[df_3['max_speed_diff'] == selected_speed_2]
+
+        # Check for missing columns or NaN values
+        print(filtered_df_2.columns)  # Debugging line
+        if filtered_df_2.empty:
+            st.error("No data available for the selected speed.")
+            return
+
+        # Ensure correct data types
+        filtered_df_2.loc[:, 'max_dist_clust'] = pd.to_numeric(filtered_df['max_dist_clust'], errors='coerce')
+        filtered_df_2.loc[:, 'All_PDR_Vector'] = pd.to_numeric(filtered_df['All_PDR_Vector'], errors='coerce')
+
+        # Drop rows with NaN values in important columns
+        filtered_df_2 = filtered_df_2.dropna(subset=['max_dist_clust', 'All_PDR_Vector'])
+
+        # Plot the Boxplot
+        fig3 = px.box(
+            filtered_df_2, 
+            x='max_dist_clust', 
+            y='All_PDR_Vector', 
+            notched=True,
+            title=f'ALL PDR Average for max_speed_diff = {selected_speed_2}',
+            labels={'ALL_PDR_avg': 'Average ALL_PDR', 'max_dist_clust': 'Maximum Distance to Cluster Head'},
+            points='all'
+        )
+        fig3.update_layout(
+            xaxis_title='Maximum Distance for Cluster member [m]', 
+            yaxis_title='Average ALL_PDR', 
+            font_size=12, 
+            title_x=0.5
+        )
+
+        fig3.update_xaxes(tickvals=df_3['max_dist_clust'].unique(), ticktext=['3', '4', '5', '10', '15'])
+        fig3.update_yaxes(range=[0.5,1])
+
+        st.plotly_chart(fig3)
         #plot_box(df_3)
 
 
